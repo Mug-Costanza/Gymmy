c3366fcb26fcd454c21851b8d3af795a6714c3d5
BigGymTim <149828775+BigGymTim@users.noreply.github.com>
2024-04-15 20:02:28 -0400

    Update admin.py
    registers chat room functions

--- gym/admin.py
+++ gym/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, UserProfile, Post
+from .models import User, UserProfile, Post, Room, Message
 
 admin.site.register(UserProfile)
 admin.site.register(Post)
 
+admin.site.register(Room)
+admin.site.register(Message)
